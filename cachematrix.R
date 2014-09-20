0a3fb7bc31ac5864727301927b14f0bf8bc7588d
Marcus E. Turnbo <marcus@emarcus.com>
2014-09-20 10:56:57 -0500

    Added .gitignore

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  
+  setInverse <- function(solve) m <<- solve
+  
+  getInverse <- function() m
+  
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)  
 }
 
 
